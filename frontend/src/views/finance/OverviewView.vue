96a10be2f2d639e748b171dbc3ccbcf1c0372c49
anton <anton@schegg.de>
2026-05-06 08:25:30 +0200

    Enhance finance views and enable docker images for arm64

--- frontend/src/views/finance/OverviewView.vue
+++ frontend/src/views/finance/OverviewView.vue
@@ -8,11 +8,15 @@ import InputText from 'primevue/inputtext'
 import Select from 'primevue/select'
 import { useOverviewStore } from '../../stores/finance/overview'
 import { useAnomalyStore } from '../../stores/finance/anomalies'
+import { useAuthStore } from '../../stores/auth'
 import type { OverviewAccount, SaveOverviewSection } from '../../api/finance'
 
 const store = useOverviewStore()
 const anomalyStore = useAnomalyStore()
+const authStore = useAuthStore()
 const router = useRouter()
+
+const canWrite = computed(() => authStore.hasPermission('finance.accounts.manage'))
 
 onMounted(() => {
   void store.refresh()
@@ -281,23 +285,26 @@ async function saveConfig() {
                   {{ acc.pending_count }}
                 </span>
               </div>
-              <div class="row-sub">{{ formatUpdatedAt(acc.balance_as_of) }}</div>
-            </div>
-            <div :class="balanceClass(acc)">{{ formatBalance(acc) }}</div>
-          </li>
-          <li
-            v-for="acc in section.accounts.filter(a => a.type_kind === 'bargeld')"
-            :key="'add-' + acc.id"
-            class="account-row add-cash-row"
-            @click.stop="router.push({ name: 'finance-transaction-new', query: { accountId: acc.id } })"
-          >
-            <div class="row-left">
-              <div class="row-title">
-                <span class="row-label text-blue-500">
-                  <i class="pi pi-plus-circle" /> Buchung hinzufügen
-                </span>
+              <div class="row-sub">
+                <template v-if="acc.type_kind === 'bargeld' && canWrite">
+                  <span
+                    class="text-blue-500 hover:underline"
+                    @click.stop="
+                      router.push({
+                        name: 'finance-transaction-new',
+                        query: { accountId: acc.id },
+                      })
+                    "
+                  >
+                    <i class="pi pi-plus-circle" /> Buchung hinzufügen
+                  </span>
+                </template>
+                <template v-else>
+                  {{ formatUpdatedAt(acc.balance_as_of) }}
+                </template>
               </div>
             </div>
+            <div :class="balanceClass(acc)">{{ formatBalance(acc) }}</div>
           </li>
         </ul>
       </section>
