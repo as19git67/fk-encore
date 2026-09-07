89ac66eecd811c90605e0b438eb1a4606dd6da20
anton <anton@schegg.de>
2026-09-07 21:28:57 +0200

    Fix SwiftUI time slider return

--- ios/Sources/FKPhotos/Features/TripPlanner/TripDayMapView.swift
+++ ios/Sources/FKPhotos/Features/TripPlanner/TripDayMapView.swift
@@ -91,7 +91,7 @@ struct TripDayMapView: View {
     }
 
     private func timeSlider(_ span: ClosedRange<Int>) -> some View {
-        VStack(alignment: .leading, spacing: 6) {
+        return VStack(alignment: .leading, spacing: 6) {
             HStack {
                 Text(TripClock.format(Int(sliderMinutes)))
                     .font(.subheadline.weight(.semibold))
